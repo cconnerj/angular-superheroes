3a63a26aa7cb65e07baef9a3934f231d28af0b38
Conner Johnson <Conner.johnson33@gmail.com>
2017-05-04 16:22:32 -0400

    added minor code

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 
 let heroData = [
   {name:"The Foot" , number:"7035554444" , affiliation:"Marvel", image:"https://github.com/hannahhoward/angular-starter-wdi/blob/master/img/princess_peach.jpg?raw=true" },
-  {name:"The Amazing Bigtoe" , number:"2354593056" , affiliation:"D.C", image:"https://github.com/hannahhoward/angular-starter-wdi/blob/master/img/storm.gif?raw=true" },
+  {name:"The Amazing Bigtoe" , number:"2354593056" , affiliation:"Dc", image:"https://github.com/hannahhoward/angular-starter-wdi/blob/master/img/storm.gif?raw=true" },
   {name: "The Unbreakable Pelvis" , number:"8439573859" , affiliation:"Marvel", image: "https://github.com/hannahhoward/angular-starter-wdi/blob/master/img/black_panther.gif?raw=true"},
-  {name: "Mr.Steve" , number:"4563568445" , affiliation:"D.C", image: "https://github.com/hannahhoward/angular-starter-wdi/blob/master/img/batman.jpg?raw=true"  }
+  {name: "Mr.Steve" , number:"4563568445" , affiliation:"Dc", image: "https://github.com/hannahhoward/angular-starter-wdi/blob/master/img/batman.jpg?raw=true"  }
 ]
 
 
-angular.module("heroApp", [])
 
 
 angular
