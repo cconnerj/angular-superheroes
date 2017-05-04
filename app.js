2a1e5b37a7dc6f555b5ec9df0b107c898c5e05ea
Conner Johnson <Conner.johnson33@gmail.com>
2017-05-04 15:19:09 -0400

    finished, no bonus

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 
 let heroData = [
-  {name:"The Foot" , number:"7035554444" , affiliation:"Marvel" },
-  {name:"The Amazing Bigtoe" , number:"2354593056" , affiliation:"D.C" },
-  {name: "The Unbreakable Pelvis" , number:"8439573859" , affiliation:"Marvel"},
+  {name:"The Foot" , number:"7035554444" , affiliation:"Marvel", image:"https://github.com/hannahhoward/angular-starter-wdi/blob/master/img/princess_peach.jpg?raw=true" },
+  {name:"The Amazing Bigtoe" , number:"2354593056" , affiliation:"D.C", image:"https://github.com/hannahhoward/angular-starter-wdi/blob/master/img/storm.gif?raw=true" },
+  {name: "The Unbreakable Pelvis" , number:"8439573859" , affiliation:"Marvel", image: "https://github.com/hannahhoward/angular-starter-wdi/blob/master/img/black_panther.gif?raw=true"},
   {name: "Mr.Steve" , number:"4563568445" , affiliation:"D.C", image: "https://github.com/hannahhoward/angular-starter-wdi/blob/master/img/batman.jpg?raw=true"  }
 ]
 
